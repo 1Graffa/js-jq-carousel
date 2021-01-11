ca66d867232f8a9f24d48401ede5828e206e0803
Giuseppe <giusepperaffa1988@gmail.com>
2021-01-11 13:57:19 +0100

    bonus + ritocchi estetici

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
   //   $(this).addClass('active');
   //   $(imgAttiva).removeClass('active');
   // });
-  $(".nav i").click(function() {
+  $(".square").click(function() {
     $(".active").removeClass("active");
     $(this).addClass("active");
     $("img").eq($(this).index()).addClass("active");
@@ -29,34 +29,34 @@ $(document).ready(function(){
 function pulsanteNext(){
 
   var imgAttiva = $('.images img.active');
-  var pallinoAttivo = $('.nav i.active');
+  var quadranteAttivo = $(".nav .active");
 
-  imgAttiva.removeClass('active');
-  imgAttiva.next().addClass('active');
+  imgAttiva.removeClass("active");
+  imgAttiva.next().addClass("active");
 
-  pallinoAttivo.removeClass('active');
-  pallinoAttivo.next().addClass('active');
+  quadranteAttivo.removeClass("active");
+  quadranteAttivo.next().addClass("active");
 
-  if(imgAttiva.hasClass('last')){
-    $('.images img.first').addClass('active');
-    $('.nav i.first').addClass('active');
+  if(imgAttiva.hasClass("last")){
+    $(".images img.first").addClass("active");
+    $(".nav .first").addClass("active");
   }
 
 }
 function pulsantePrev(){
 
-  var imgAttiva = $('.images img.active');
-  var pallinoAttivo = $('.nav i.active');
+  var imgAttiva = $(".images img.active");
+  var quadranteAttivo = $(".nav .active");
 
-  imgAttiva.removeClass('active');
-  imgAttiva.prev().addClass('active');
+  imgAttiva.removeClass("active");
+  imgAttiva.prev().addClass("active");
 
-  pallinoAttivo.removeClass('active');
-  pallinoAttivo.prev().addClass('active');
+  quadranteAttivo.removeClass("active");
+  quadranteAttivo.prev().addClass("active");
 
-  if(imgAttiva.hasClass('first')){
-    $('.images img.last').addClass('active');
-    $('.nav i.last').addClass('active');
+  if(imgAttiva.hasClass("first")){
+    $(".images img.last").addClass("active");
+    $(".nav .last").addClass("active");
   }
 
 }
